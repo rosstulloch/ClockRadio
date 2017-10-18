669fe17c4291756ec38a858cc67891a26ab43545
rosstulloch <rosst@zeroonetwenty.com>
2017-10-19 09:03:37 +1100

    Removed dead code.

--- Source/ViewAdditions.swift
+++ Source/ViewAdditions.swift
@@ -11,41 +11,11 @@ import UIKit
 
 extension UIView
 {
-/*    func labelWithTag( _ tag:Int ) -> UILabel {
-        return self.viewWithTag(tag) as! UILabel
-    }   */
-    
-/*    func positionHorizontallyOffsetFrom( _ otherView:UIView, space:CGFloat ) {
-        self.frame.origin.x = otherView.frame.origin.x + space
-    }   */
-    
- //   func positionFromTop( space:CGFloat ) {
- //       self.frame.origin.y = self.superview!.frame.origin
-  //  }
     
     func positionHorizontallyCentered() {
         self.sizeToFit()
         self.frame.origin.x = (self.superview!.frame.width/2) - (self.frame.width/2)
     }
-
-/*    func positionVerticallyCentered() {
-        self.sizeToFit()
-        self.frame.origin.y = (self.superview!.frame.height/2) - (self.frame.height/2)
-    }   */
-
- /*   func positionAtBottom() {
-        self.sizeToFit()
-        self.frame.origin.y = self.superview!.frame.height - self.frame.height
-    }   */
-    
-/*    func positionBelow( _ otherView:UIView, space:CGFloat = 0 ) {
-        self.sizeToFit()
-        self.frame.origin.y = otherView.frame.origin.y + otherView.frame.height + space
-    }
-    
-    func positionLeftJustified() {
-        self.frame.origin.x = 0
-    }   */
     
     func positionDownByThird( _ fraction:Int = 1 ) {
         let third = self.superview!.frame.height / 3
